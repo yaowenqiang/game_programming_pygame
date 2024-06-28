ef90c9ddf30739213178e42b41197b8f2dfcefff
yaowenqiang <yaowenqiang111@163.com>
2024-06-28 23:38:05 +0800

    add text move with mouse demo

--- main.py
+++ main.py
@@ -21,12 +21,23 @@ while game_running:
         if event.type == pygame.QUIT:
             game_running = False
     screen.fill((0, 0, 0))
+
+    mouse_position = pygame.mouse.get_pos()
+    x, y = mouse_position
+    if x + hello_world_size[0] > 800:
+        x = 800 - hello_world_size[0]
+    if y + hello_world_size[1] > 600:
+        y = 600 - hello_world_size[1]
+
+
+
+
+    # x += 5 * direction_x
+    # y += 5 * direction_x
+    # if x + hello_world_size[0] > 800 or x < 0:
+    #     direction_x *= -1
+    # if y + hello_world_size[1] > 600 or y < 0:
+    #     direction_y *= -1
     screen.blit(hello_world, (x, y))
-    x += 5 * direction_x
-    y += 5 * direction_x
-    if x + hello_world_size[0] > 800 or x < 0:
-        direction_x *= -1
-    if y + hello_world_size[1] > 600 or y < 0:
-        direction_y *= -1
     pygame.display.update()
 pygame.quit()
